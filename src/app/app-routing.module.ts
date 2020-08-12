008ae1a768113be335519ba4d5157ec35abf320d
Sachinda <sachinda.n@arpicoinsurance.com>
2020-08-12 23:39:57 +0530

    congi dashboard design

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {LoginComponent} from './login/login.component';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {DashboardMainComponent} from './dashboard-main/dashboard-main.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : '', component: LoginComponent},
+  {path : 'dashboard', component: DashboardComponent,
+  children:[
+    { path: '', redirectTo: 'dashboardmain', pathMatch: 'full' },
+    {
+      path: 'main',
+      pathMatch : 'full',
+      redirectTo: '/dashboardmain'
+    },
+    {path : 'dashboardmain' , component : DashboardMainComponent}
+  ]},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
